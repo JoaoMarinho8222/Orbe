16d4ad978120221654fb19644b4c3f5301f6e3af
JoaoMarinho8222 <89cjzr527n@privaterelay.appleid.com>
2026-09-22 20:00:13 -0300

    chore: bump service worker cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="orbe-shell-v10";
+const CACHE="orbe-shell-v11";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
 self.addEventListener("install",event=>{
   event.waitUntil(
