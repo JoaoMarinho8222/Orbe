4c16f77edb9b96bc1a157bd5ca8d3b78909887bc
JoaoMarinho8222 <89cjzr527n@privaterelay.appleid.com>
2026-09-22 19:41:04 -0300

    Refresh PWA shell after Orbe home simplification

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="orbe-shell-v7";
+const CACHE="orbe-shell-v8";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
 self.addEventListener("install",event=>{
   event.waitUntil(
