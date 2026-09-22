86b0b21d6342a24546daae583fd3f6130ba39bc3
JoaoMarinho8222 <89cjzr527n@privaterelay.appleid.com>
2026-09-22 19:47:59 -0300

    Refresh PWA for darker ambient palette

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="orbe-shell-v9";
+const CACHE="orbe-shell-v10";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
 self.addEventListener("install",event=>{
   event.waitUntil(
